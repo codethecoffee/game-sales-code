01909a7daa2632811ed9c09c8ecc6c83fb86d24e
sujunglee <sujunglee2020@u.northwestern.edu>
2018-11-17 15:15:37 -0600

    Better decision tree with 75% accuracy

--- parser.py
+++ parser.py
@@ -24,9 +24,31 @@ def parse(filename):
         sales = sorted([NA, EU, JP, Other], key=lambda x: x[0], reverse=True)
         # Greatest_Sales: the region the game sold the most copies in
         game["Greatest_Sales"] = sales[0][1]
-        # print(game + "\n")
+        deleteFields(game)
+        print(game)
+        print("\n")
 
     return raw_data
+
+
+def deleteFields(entry):
+    # Delete the name for now
+    # Add ability to parse the name/bag of words?
+    del entry["Name"]
+
+    del entry["Platform"]
+    del entry["NA_Sales"]
+    del entry["EU_Sales"]
+    del entry["JP_Sales"]
+    del entry["Other_Sales"]
+    del entry["Global_Sales"]
+
+    # TODO: Somehow accommodate for continuous values like
+    # scores in our binary decision tree
+    del entry["Critic_Score"]
+    del entry["User_Score"]
+    del entry["User_Count"]
+    del entry["Critic_Count"]
 
 
 if __name__ == "__main__":
--- unit_tests.py
+++ unit_tests.py
@@ -10,38 +10,38 @@ def testPruningOnData(inFile):
     withoutPruning = []
     data = parse(inFile)
     # print(data)
-    # for i in range(3):
+    for i in range(3):
+        random.shuffle(data)
+        train = data[:len(data)//2]
+        #print("Train: {}".format(train))
+        valid = data[len(data)//2:3*len(data)//4]
+        test = data[3*len(data)//4:]
 
-    random.shuffle(data)
-    train = data[:len(data)//2]
-    #print("Train: {}".format(train))
-    valid = data[len(data)//2:3*len(data)//4]
-    test = data[3*len(data)//4:]
+        tree = ID3.ID3(train, 'NA')
+        acc = ID3.test(tree, train)
+        print("training accuracy: ", acc)
+        acc = ID3.test(tree, valid)
+        print("validation accuracy: ", acc)
+        acc = ID3.test(tree, test)
+        print("test accuracy: ", acc)
 
-    tree = ID3.ID3(train, 'NA')
-    acc = ID3.test(tree, train)
-    print("training accuracy: ", acc)
-    acc = ID3.test(tree, valid)
-    print("validation accuracy: ", acc)
-    acc = ID3.test(tree, test)
-    print("test accuracy: ", acc)
+        ID3.prune(tree, valid)
+        acc = ID3.test(tree, train)
+        print("pruned tree train accuracy: ", acc)
+        acc = ID3.test(tree, valid)
+        print("pruned tree validation accuracy: ", acc)
+        acc = ID3.test(tree, test)
+        print("pruned tree test accuracy: ", acc)
+        withPruning.append(acc)
+        tree = ID3.ID3(train+valid, 'NA')
+        print("Finished ID3!")
+        acc = ID3.test(tree, test)
+        print("Finished calling test")
+        print("no pruning test accuracy: ", acc)
+        withoutPruning.append(acc)
+        print(withPruning)
+        print(withoutPruning)
 
-    ID3.prune(tree, valid)
-    acc = ID3.test(tree, train)
-    print("pruned tree train accuracy: ", acc)
-    acc = ID3.test(tree, valid)
-    print("pruned tree validation accuracy: ", acc)
-    acc = ID3.test(tree, test)
-    print("pruned tree test accuracy: ", acc)
-    withPruning.append(acc)
-    tree = ID3.ID3(train+valid, 'NA')
-    print("Finished ID3!")
-    acc = ID3.test(tree, test)
-    print("Finished calling test")
-    print("no pruning test accuracy: ", acc)
-    withoutPruning.append(acc)
-    print(withPruning)
-    print(withoutPruning)
     print("average with pruning", sum(withPruning)/len(withPruning),
           " without: ", sum(withoutPruning)/len(withoutPruning))
 
