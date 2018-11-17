a814d460a6011854f85dc7fd4c5b7d104d47d561
sujunglee <sujunglee2020@u.northwestern.edu>
2018-11-17 14:16:12 -0600

    Pre-process data to have greatest_sales field

--- parser.py
+++ parser.py
@@ -1,18 +1,33 @@
+import csv
+
+
 def parse(filename):
     '''
     takes a filename and returns attribute information
     '''
     # initialize variables
 
-    out = []
+    raw_data = []
     csvfile = open(filename, 'r')
     fileToRead = csv.reader(csvfile)
 
     headers = next(fileToRead)
 
-    # iterate through rows of actual data
     for row in fileToRead:
-        out.append(dict(zip(headers, row)))
+        raw_data.append(dict(zip(headers, row)))
 
-    print(out)
-    return out
+    for game in raw_data:
+        NA, EU = (game["NA_Sales"], "NA"), (game["EU_Sales"], "EU")
+        JP, Other = (game["JP_Sales"],
+                     "JP"), (game["Other_Sales"], "Other")
+
+        sales = sorted([NA, EU, JP, Other], key=lambda x: x[0], reverse=True)
+        # Greatest_Sales: the region the game sold the most copies in
+        game["Greatest_Sales"] = sales[0][1]
+        # print(game + "\n")
+
+    return raw_data
+
+
+if __name__ == "__main__":
+    parse("data/game_data.csv")
